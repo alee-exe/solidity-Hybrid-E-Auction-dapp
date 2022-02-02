d8c7a75658607004506b5427ce54b563ad08fb3f
Andy <enquiries.alee@protonmail.com>
2022-02-02 19:02:29 +0000

    Added search filter for listings

--- client/components/AuctionListing.js
+++ client/components/AuctionListing.js
@@ -7,12 +7,17 @@ import Link from 'next/link';
 
 
 export default class AuctionListingComponent extends Component {
-    
+
     state = {
         auctionListing: null,
         web3Provider: null,
         userAccount: null,
-        contract: null
+        contract: null,
+        selectedFilter: "All",
+        userOwnedAuctions: null,
+        userOwnedAuctionIds: null,
+        userNotOwnedAuctions: null,
+        userNotOwnedAuctionIds: null
     };
 
     ROUTE_AUCTION_ID = "auctions/view/[id]";
@@ -25,11 +30,34 @@ export default class AuctionListingComponent extends Component {
             const contract = new web3.eth.Contract(AuctionListing['abi'], contractAddress);
             const userAddresses = await web3.eth.getAccounts();
             const auctionListing = await contract.methods.getListedAuctions().call();
+            const userAccount = userAddresses[0];
 
-            this.setState({ auctionListing, web3Provider: web3, userAccount: userAddresses[0], contract });
+            let userOwnedAuctions = [];
+            let userOwnedAuctionIds = [];
+            let userNotOwnedAuctions = [];
+            let userNotOwnedAuctionIds = [];
+
+            for (let id = 0; id < auctionListing.length; id++) {
+                const auctionOwner = await contract.methods.getOwner(id).call();
+
+                // Check which auctions the user is the owner of and the id of the auction
+                if (userAccount === auctionOwner) {
+                    userOwnedAuctions.push(auctionListing[id]);
+                    userOwnedAuctionIds.push(id);
+                } else {
+                    userNotOwnedAuctions.push(auctionListing[id]);
+                    userNotOwnedAuctionIds.push(id);
+                };
+            };
+
+            this.setState({ auctionListing, web3Provider: web3, userAccount, contract, userOwnedAuctions, userOwnedAuctionIds, userNotOwnedAuctions, userNotOwnedAuctionIds });
         } catch (error) {
             console.log(error);
         };
+    }
+
+    handleFilterSelection = (event) => {
+        this.setState({ selectedFilter: event.target.value });
     }
 
     render() {
@@ -42,19 +70,48 @@ export default class AuctionListingComponent extends Component {
                             <hr className="border-slate-400 pb-4" />
 
                             <div className="border flex pl-2 mb-4">
-                                <h1>Showing {this.state.auctionListing.length} auctions in total.</h1>
+                                <h1>Filter by:  <select id="filter" className="bg-slate-500 text-white shadow-lg font-bold" onChange={this.handleFilterSelection}>
+                                    <option value="All">All</option>
+                                    <option value="Owned">Owned</option>
+                                    <option value="Not Owned">Not Owned</option>
+                                </select></h1>
                             </div>
 
-                            <div>
+                            <div className="border flex pl-2 mb-4">
+                                {this.state.selectedFilter === "All" ? (<h1>Showing {this.state.auctionListing.length} auctions in total.</h1>) : null}
+                                {this.state.selectedFilter === "Owned" ? (<h1>Showing {this.state.userOwnedAuctions.length} auctions in total.</h1>) : null}
+                                {this.state.selectedFilter === "Not Owned" ? (<h1>Showing {this.state.userNotOwnedAuctions.length} auctions in total.</h1>) : null}
+                            </div>
+
+                            {this.state.selectedFilter === "All" ? (<div>
                                 {this.state.auctionListing.map((auction, idx) => (
-                                    <div key = {idx}>
+                                    <div key={idx}>
                                         <Link href={{ pathname: this.ROUTE_AUCTION_ID, query: { id: idx + 1 } }}>
                                             <a><span className="pl-2 border flex bg-slate-300 font-semibold">Auction Contract: {auction}</span>
                                                 <AuctionPreview web3={this.state.web3Provider} id={idx} contract={this.state.contract}></AuctionPreview></a></Link>
                                     </div>
                                 ))}
-                            </div>
+                            </div>) : null}
 
+                            {this.state.selectedFilter === "Owned" ? (<div>
+                                {this.state.userOwnedAuctions.map((auction, idx) => (
+                                    <div key={idx}>
+                                        <Link href={{ pathname: this.ROUTE_AUCTION_ID, query: { id: this.state.userOwnedAuctionIds[this.state.userOwnedAuctions.indexOf(auction)] + 1 } }}>
+                                            <a><span className="pl-2 border flex bg-slate-300 font-semibold">Auction Contract: {auction}</span>
+                                                <AuctionPreview web3={this.state.web3Provider} id={this.state.userOwnedAuctionIds[this.state.userOwnedAuctions.indexOf(auction)]} contract={this.state.contract}></AuctionPreview></a></Link>
+                                    </div>
+                                ))}
+                            </div>) : null}
+
+                            {this.state.selectedFilter === "Not Owned" ? (<div>
+                                {this.state.userNotOwnedAuctions.map((auction, idx) => (
+                                    <div key={idx}>
+                                        <Link href={{ pathname: this.ROUTE_AUCTION_ID, query: { id: this.state.userNotOwnedAuctionIds[this.state.userNotOwnedAuctions.indexOf(auction)] + 1 } }}>
+                                            <a><span className="pl-2 border flex bg-slate-300 font-semibold">Auction Contract: {auction}</span>
+                                                <AuctionPreview web3={this.state.web3Provider} id={this.state.userNotOwnedAuctionIds[this.state.userNotOwnedAuctions.indexOf(auction)]} contract={this.state.contract}></AuctionPreview></a></Link>
+                                    </div>
+                                ))}
+                            </div>) : null}
                         </div>
                     </div>
                 )}
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,33 +3,33 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <div>
-            <h1 className="text-4xl font-bold pb-4">Decentralized Auction App</h1>
+            <h1 className="text-4xl font-bold pb-4">Hybrid CReam Decentralized E-Auction</h1>
             <hr className="pb-4 border-slate-400" />
 
             <nav className="pb-4">
                 <div className="flex mt-4">
                     <Link href="/">
-                        <a className="mr-4 font-bold text-blue-500">
+                        <a className="mr-8 font-bold text-blue-500 text-base">
                             Auction Listings
                         </a>
                     </Link>
                     <Link href="/create-auction">
-                        <a className="mr-6 font-bold text-blue-500">
+                        <a className="mr-8 font-bold text-blue-500 text-base">
                             Create Auction
                         </a>
                     </Link>
                     <Link href="/my-auction-activity">
-                        <a className="mr-6 font-bold text-blue-500">
+                        <a className="mr-8 font-bold text-blue-500 text-base">
                             My Auction Activity
                         </a>
                     </Link>
                     <Link href="/my-current-bids">
-                        <a className="mr-6 font-bold text-blue-500">
+                        <a className="mr-8 font-bold text-blue-500 text-base">
                             My Current Bids
                         </a>
                     </Link>
                     <Link href="/user-dashboard">
-                        <a className="mr-6 font-bold text-blue-500">
+                        <a className="mr-8 font-bold text-blue-500 text-base">
                             User Dashboard
                         </a>
                     </Link>
